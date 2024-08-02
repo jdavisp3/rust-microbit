22da784fd5b17813d2517dc9967ec67cf3315a18
Dave Peticolas <dave@krondo.com>
2024-08-02 16:56:23 -0700

    modularity

--- src/main.rs
+++ src/main.rs
@@ -10,71 +10,22 @@ use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
 mod display;
 
-const LETTER_D: [[u8; 5]; 5] = [
-    [0, 1, 1, 0, 0],
-    [0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0],
-    [0, 1, 1, 0, 0],
-];
-
-const LETTER_L: [[u8; 5]; 5] = [
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-];
-
-const LETTER_P: [[u8; 5]; 5] = [
-    [0, 1, 1, 1, 0],
-    [0, 1, 0, 1, 0],
-    [0, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-];
-
-const LETTER_W: [[u8; 5]; 5] = [
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
-];
-
-const COLON: [[u8; 5]; 5] = [
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-];
-
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
         let mut display: Display = Display::new(board.display_pins);
 
-        #[allow(non_snake_case)]
-        let heart = [
-            [0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
         loop {
-            display.show(&mut timer, LETTER_P, 2000);
-            display.show(&mut timer, LETTER_W, 2000);
-            display.show(&mut timer, LETTER_L, 2000);
-            display.show(&mut timer, COLON, 2000);
-            display.show(&mut timer, LETTER_P, 2000);
-            display.show(&mut timer, LETTER_D, 2000);
+            display.show(&mut timer, display::LETTER_P, 2000);
+            display.show(&mut timer, display::LETTER_W, 2000);
+            display.show(&mut timer, display::LETTER_L, 2000);
+            display.show(&mut timer, display::COLON, 2000);
+            display.show(&mut timer, display::LETTER_P, 2000);
+            display.show(&mut timer, display::LETTER_D, 2000);
             display.show(&mut timer, display::LETTER_X, 2000);
             display.clear();
-            display.show(&mut timer, heart, 2000);
+            display.show(&mut timer, display::HEART, 2000);
             timer.delay_ms(2500_u32);
         }
     }
