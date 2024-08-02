fd3c77a07ea8889806a823c4355a0308a3be1f73
Dave Peticolas <dave@krondo.com>
2024-08-02 16:20:59 -0700

    use constant

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,19 @@ use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
+const LETTER_P: [[u8; 5]; 5] = [
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+];
+
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
         let mut display: Display = Display::new(board.display_pins);
-
-        #[allow(non_snake_case)]
-        let letter_P = [
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-        ];
 
         #[allow(non_snake_case)]
         let letter_W = [
@@ -67,16 +66,27 @@ fn main() -> ! {
             [0, 1, 0, 1, 0],
             [1, 0, 0, 0, 1],
         ];
+
+        #[allow(non_snake_case)]
+        let heart = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
         loop {
-            display.show(&mut timer, letter_P, 2000);
+            display.show(&mut timer, LETTER_P, 2000);
             display.show(&mut timer, letter_W, 2000);
             display.show(&mut timer, letter_L, 2000);
             display.show(&mut timer, colon, 2000);
-            display.show(&mut timer, letter_P, 2000);
+            display.show(&mut timer, LETTER_P, 2000);
             display.show(&mut timer, letter_D, 2000);
             display.show(&mut timer, letter_X, 2000);
             display.clear();
-            timer.delay_ms(250_u32);
+            display.show(&mut timer, heart, 2000);
+            timer.delay_ms(2500_u32);
         }
     }
 
