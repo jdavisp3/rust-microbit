83e14646eed6b1471c9b38bab2e12e8b8895cd6f
Dave Peticolas <dave@krondo.com>
2024-08-02 16:51:36 -0700

    const + module

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
+mod display;
+
 const LETTER_D: [[u8; 5]; 5] = [
     [0, 1, 1, 0, 0],
     [0, 1, 0, 1, 0],
@@ -55,15 +57,6 @@ fn main() -> ! {
         let mut display: Display = Display::new(board.display_pins);
 
         #[allow(non_snake_case)]
-        let letter_X = [
-            [1, 0, 0, 0, 1],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [1, 0, 0, 0, 1],
-        ];
-
-        #[allow(non_snake_case)]
         let heart = [
             [0, 1, 0, 1, 0],
             [1, 0, 1, 0, 1],
@@ -79,7 +72,7 @@ fn main() -> ! {
             display.show(&mut timer, COLON, 2000);
             display.show(&mut timer, LETTER_P, 2000);
             display.show(&mut timer, LETTER_D, 2000);
-            display.show(&mut timer, letter_X, 2000);
+            display.show(&mut timer, display::LETTER_X, 2000);
             display.clear();
             display.show(&mut timer, heart, 2000);
             timer.delay_ms(2500_u32);
