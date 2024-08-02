88531cf1bd6fa7f72e3f8898fb4a623ebc2be274
Dave Peticolas <dave@krondo.com>
2024-08-01 19:38:29 -0700

    pwl:pdx

--- src/main.rs
+++ src/main.rs
@@ -12,67 +12,69 @@ use microbit::{board::Board, display::blocking::Display, hal::Timer};
 fn main() -> ! {
     if let Some(board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
-        let mut display = Display::new(board.display_pins);
+        let mut display: Display = Display::new(board.display_pins);
 
         #[allow(non_snake_case)]
-        let letter_I = [
+        let letter_P = [
             [0, 1, 1, 1, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_W = [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_L = [
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
             [0, 1, 1, 1, 0],
         ];
 
-        let heart = [
+        #[allow(non_snake_case)]
+        let colon = [
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_D = [
+            [0, 1, 1, 0, 0],
             [0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_X = [
             [1, 0, 0, 0, 1],
             [0, 1, 0, 1, 0],
             [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_R = [
-            [0, 1, 1, 0, 0],
             [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_u = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_s = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 1, 1, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_t = [
-            [0, 0, 1, 0, 0],
-            [0, 1, 1, 1, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
+            [1, 0, 0, 0, 1],
         ];
         loop {
-            display.show(&mut timer, letter_I, 1000);
-            display.show(&mut timer, heart, 1000);
-            display.show(&mut timer, letter_R, 1000);
-            display.show(&mut timer, letter_u, 1000);
-            display.show(&mut timer, letter_s, 1000);
-            display.show(&mut timer, letter_t, 1000);
+            display.show(&mut timer, letter_P, 2000);
+            display.show(&mut timer, letter_W, 2000);
+            display.show(&mut timer, letter_L, 2000);
+            display.show(&mut timer, colon, 2000);
+            display.show(&mut timer, letter_P, 2000);
+            display.show(&mut timer, letter_D, 2000);
+            display.show(&mut timer, letter_X, 2000);
             display.clear();
             timer.delay_ms(250_u32);
         }
