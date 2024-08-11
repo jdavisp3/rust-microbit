88a3751dc00bb285aa09a95fbc4784fccbd26ab7
Dave Peticolas <dave@krondo.com>
2024-08-11 16:28:36 -0700

    moar debugging

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,11 @@ fn main() -> ! {
         let mut timer = Timer::new(board.TIMER0);
         let mut display: Display = Display::new(board.display_pins);
         let display_state = display::init();
-
-         let mut scroll_string = |s: &str| {
+ 
+        loop {
+            let s = "MYRIAD GENETICS ❤";
             let last_x = (s.chars().count() as i32 * 5) + 1;
-            for x in -5..last_x {
+            for x in -3..last_x {
                 let mut screen: display::DisplayBuffer = [[0; 5]; 5];
                  for row in 0..5 {
                     for col in 0..5 {
@@ -26,17 +27,15 @@ fn main() -> ! {
                         let mut bit: u8 = 0;
                         if char_index >= 0 && char_index < s.len() as i32 {
                             let col_offset = (x + col) % 5;
-                            bit = display::getchar(&display_state, s.chars().nth(char_index as usize).unwrap())[row as usize][col_offset as usize];
+                            let c: char = s.chars().nth(char_index as usize).unwrap();
+                            let display_c: display::DisplayBuffer = display::getchar(&display_state, c);
+                            bit = display_c[row as usize][col_offset as usize];
                         }
                         screen[row as usize][col as usize] = bit;
                     }
                 }
                 display.show(&mut timer, screen, 1000);
             }
-        };
- 
-        loop {
-            scroll_string("MYRIAD GENETICS ❤");
         }
     }
 
