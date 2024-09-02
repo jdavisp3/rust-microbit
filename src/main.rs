eb5aa13f108714b07c4ca33cffcd3c78f77a34a4
Dave Peticolas <dave@krondo.com>
2024-09-02 11:28:00 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,15 @@ mod display_helper;
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
-        let mut timer = Timer::new(board.TIMER0);
+        let mut timer: Timer<microbit::pac::TIMER0> = Timer::new(board.TIMER0);
         let mut display: Display = Display::new(board.display_pins);
-        let helper = display_helper::init();
+        let helper: display_helper::DisplayHelper = display_helper::init();
  
         loop {
             let s: &str = " MYRIAD GENETICS ❤";
             let last_x: usize = helper.get_scroll_width(s);
             for x in 0..last_x {
-                let screen = helper.get_display_buffer_at_col(s, x);
+                let screen: display_helper::DisplayBuffer = helper.get_display_buffer_at_col(s, x);
                 display.show(&mut timer, screen, 200);
             }
         }
