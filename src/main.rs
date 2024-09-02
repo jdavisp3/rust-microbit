05c5119a5ba11c2f9c5e57240883db248ffbb2e8
Dave Peticolas <dave@krondo.com>
2024-09-02 10:05:08 -0700

    rename module

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ use panic_halt as _;
 use cortex_m_rt::entry;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
-mod display;
+mod display_helper;
 
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
         let mut display: Display = Display::new(board.display_pins);
-        let display_helper = display::init();
+        let display_helper = display_helper::init();
  
         loop {
             let s = " MYRIAD GENETICS ❤";
             let last_x = (s.chars().count() as i32 * 5) + 1;
             for x in 0..last_x {
-                let mut screen: display::DisplayBuffer = [[0; 5]; 5];
+                let mut screen: display_helper::DisplayBuffer = [[0; 5]; 5];
                  for row in 0..5 {
                     for col in 0..5 {
                         let char_index = (x + col).div_euclid(5);
@@ -28,7 +28,7 @@ fn main() -> ! {
                         if char_index >= 0 && char_index < s.chars().count() as i32 {
                             let col_offset = (x + col).rem_euclid(5);
                             let c: char = s.chars().nth(char_index as usize).unwrap();
-                            let display_c: display::DisplayBuffer = display_helper.getchar(c).buffer;
+                            let display_c: display_helper::DisplayBuffer = display_helper.getchar(c).buffer;
                             bit = display_c[row as usize][col_offset as usize];
                         }
                         screen[row as usize][col as usize] = bit;
